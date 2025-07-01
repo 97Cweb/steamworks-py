1b0663585c92bfa1aa4068436377ecec385d0a13
Collin Barker <97cweb@gmail.com>
2025-07-01 19:59:58 -0400

    add so file

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name="steamworks",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     include_package_data=True,
-    package_data={"steamworks": ["*.dll", "*.txt"]},
+    package_data={"steamworks": ["*.dll", "*.txt","*.so"]},
 )
 
